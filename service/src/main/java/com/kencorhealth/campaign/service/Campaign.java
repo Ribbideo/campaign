e982242b566fcf4d542e406a10fe81abdf43bc37
Srinivas Veeravalli <srini@kencorhealth.com>
2019-10-14 23:15:06 +0530

    Compile error fix

--- service/src/main/java/com/kencorhealth/campaign/service/Campaign.java
+++ service/src/main/java/com/kencorhealth/campaign/service/Campaign.java
@@ -1,5 +1,6 @@
 package com.kencorhealth.campaign.service;
 
+import ca.grimoire.dropwizard.cors.CorsBundle;
 import com.codahale.metrics.health.HealthCheck;
 import com.codahale.metrics.health.HealthCheckRegistry;
 import com.kencorhealth.campaign.cdn.CDNUtil;
@@ -20,10 +21,6 @@ import io.dropwizard.auth.AuthDynamicFeature;
 import io.dropwizard.auth.AuthValueFactoryProvider;
 import io.dropwizard.auth.oauth.OAuthCredentialAuthFilter;
 import io.dropwizard.jersey.setup.JerseyEnvironment;
-import java.util.EnumSet;
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 public class Campaign extends Application<CampaignConfig>
@@ -39,13 +36,11 @@ public class Campaign extends Application<CampaignConfig>
 
     @Override
     public void initialize(Bootstrap<CampaignConfig> bootstrap) {
-        // Empty for now
+        bootstrap.addBundle(new CorsBundle<>());
     }
 
     @Override
     public void run(CampaignConfig cc, Environment e) throws Exception {
-        enableCorsHeaders(e);
-        
         CampaignFactory.init(cc.getMongo().getUri());
         
         final MongoHealthCheck mhc =
@@ -89,12 +84,4 @@ public class Campaign extends Application<CampaignConfig>
             new AuthValueFactoryProvider.Binder(AuthToken.class)
         );
     }
-    
-    private void enableCorsHeaders(Environment e) {
-        FilterRegistration.Dynamic corsFilter = e.servlets().addFilter("CORS", CrossOriginFilter.class);
-        corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
-        corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-        corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
-        corsFilter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
-    }        
 }
